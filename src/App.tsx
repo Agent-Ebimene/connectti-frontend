b63ddd0058e08f3abc5cd78449fd98289e3b083a
Agent-Ebimene <ebimeneagent@gmail.com>
2024-05-28 14:06:03 +0100

    Set up connectti frontend

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus dolorem voluptas quidem, cupiditate perferendis sapiente ex maxime aspernatur! Consequuntur eum doloremque sapiente! Est iure dignissimos corrupti ipsam quod quae totam aspernatur et in quis expedita veritatis, necessitatibus cum dicta explicabo modi repellendus excepturi atque provident mollitia. Fuga est voluptate quod!
+      </div>
+      <Button variant='destructive' className=''>Click Me!</Button>
+    </>
+  )
+}
+
+export default App
